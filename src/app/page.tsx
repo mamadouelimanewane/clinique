e9085753a487e10a969d242fb7b03e8680a75331
Mamadou Elimane Wane <votre.email@exemple.com>
2026-01-11 22:12:08 +0100

    fix: set landing page as home and fix 404 at root

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import { redirect } from "next/navigation";
+import PublicWebsite from "@/components/public/site-web";
 
 export default function Home() {
-  redirect("/dashboard");
+  return <PublicWebsite />;
 }
